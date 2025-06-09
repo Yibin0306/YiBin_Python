1d278aeb46ccebc96696af31c8e1659186f07d07
Yibin0306 <972164359@qq.com>
2025-06-09 13:59:26 +0800

    更新

---
+++ 第一阶段/第1章-初始Python/实用快捷键.py
@@ -0,0 +1,12 @@
+"""
+PyCharm中多行缩进的快捷键为Tab（增加缩进）和Shift + Tab（减少缩进）
+shift + f10 运行程序
+ctrl + d 复制单行内容
+ctrl + y 删除单行内容
+shift + esc 隐藏控制台
+ctrl + p 显示方法的参数信息
+ctrl + q 显示文档字符串（Docstring）
+ctrl + / 注释/取消注释当前行
+ctrl + b 或 ctrl + 左键点击 跳转到定义
+ctrl + K： 提交更改
+"""
