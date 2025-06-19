245e733c623a64048eccab1de8f90b82153b85d7
Yibin0306 <972164359@qq.com>
2025-06-19 15:44:35 +0800

    更新

--- 第一阶段/第7章-Python函数进阶/01-函数的多返回值.py
+++ 第一阶段/第7章-Python函数进阶/01-函数的多返回值.py
@@ -1,5 +1,6 @@
 # 函数多返回值
 def test_return():
-    return 1,2
-x,y = test_return()
-print(f"第一个返回值是{x}，第二个返回值是{y}")
+    return 1,'hello',True
+x,y,z = test_return()
+print(f"第一个返回值是{x}，第二个返回值是{y}，第三个返回值是{z}")
+
