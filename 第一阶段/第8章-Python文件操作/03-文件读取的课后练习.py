894e7d4a7ec509f82991142cb445fba21bfede73
Yibin0306 <972164359@qq.com>
2025-06-20 12:01:51 +0800

    更新

--- 第一阶段/第8章-Python文件操作/03-文件读取的课后练习.py
+++ 第一阶段/第8章-Python文件操作/03-文件读取的课后练习.py
@@ -9,6 +9,18 @@ for line in f:
 print(num)
 """
 # 教程写
+# 直接使用count方法统计
 f = open("D:/word.txt","r",encoding="utf-8")
-num = f.read().count("itheima")
+# num = f.read().count("itheima")
+# print(num)
+
+# 一行一行读取
+num = 0
+for line in f:
+    line = line.strip()
+    words = line.split(" ")
+    for word in words:
+        if word == "itheima":
+            num += 1
 print(num)
+f.close()
